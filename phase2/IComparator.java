5c83cf47cdbb1998b9586ea261ee2d8a124f59e6
Jialin Gao <gao.j@husky.neu.edu>
2017-10-26 22:30:10 -0400

    Completed phase B

---
+++ phase2/IComparator.java
@@ -0,0 +1,10 @@
+
+// Defines common methods for each layers
+
+public interface LayerUtils {
+
+  // Generates report for Driver class to determine if
+  // the plagiarism is detected.
+  String generateReport(String programA, String programB);
+
+}
