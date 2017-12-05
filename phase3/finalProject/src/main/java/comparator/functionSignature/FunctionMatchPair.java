c3231b4f4beb150f306e45d5781ddd4b3fa958d6
sumeetdubey <sumeetdubey@ccs.neu.edu>
2017-12-04 22:09:06 -0500

    added comments

--- phase3/finalProject/src/main/java/comparator/functionSignature/FunctionMatchPair.java
+++ phase3/finalProject/src/main/java/comparator/functionSignature/FunctionMatchPair.java
@@ -1,9 +1,5 @@
 package comparator.functionSignature;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-
 /**
  * @author sumeetdubey
  * Class for a function match pair. The class has two objects that stores the function signatures of 
@@ -29,26 +25,6 @@ public class FunctionMatchPair {
 		sb.append(fs1.getName() +" from program 1 matches with " +fs2.getName() +" from program 2\n");
 		return sb.toString();
 	}
-
-//	/**
-//	 * Generates a string of this function signature arguments
-//	 * @param sb
-//	 */
-//	private void generateArgumentString(StringBuilder sb, FunctionSignature fs) {
-//		Iterator<Entry<String, Integer>> it = fs.getArgs().entrySet().iterator();
-//		while(it.hasNext()) {
-//			Map.Entry<String, Integer> pair=(Map.Entry<String, Integer>) it.next();
-//			if(pair.getKey().equals("none")) {
-//				sb.append(" having no arguments");
-//			}
-//			else {
-//				sb.append(" having " + pair.getValue() + " argument of " +pair.getKey() +" type");
-//				
-//			}
-//			sb.append("\t");
-//		}
-//		sb.append("\n");
-//	}
 	
 	private FunctionSignature fs1;
 	private FunctionSignature fs2;
