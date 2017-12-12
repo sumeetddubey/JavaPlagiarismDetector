e35d71dae29d5c2f4304969ec76cb9aa2a9f2124
sumeetdubey <sumeetdubey@ccs.neu.edu>
2017-12-12 14:50:23 -0500

    rearranged project directory structure

--- phase2/IComparator.java
+++
@@ -1,9 +0,0 @@
-
-// Defines common methods for each comparison
-
-public interface IComparator {
-
-  // Returns generated report of given prograams to PlagiarismDetector
-  String generateReport(String programA, String programB);
-
-}
